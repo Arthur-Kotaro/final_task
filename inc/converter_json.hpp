fd67fc8b20f1d4cb86509aa1d3c0522dd9f93c5c
Arthur-Kotaro <arthur.kotaro@yandex.ru>
2025-12-05 11:31:33 +0000

    version c

--- inc/converter_json.hpp
+++ inc/converter_json.hpp
@@ -1,17 +1,17 @@
 #pragma once
 
 #include <cstddef>
-#include <exception>
+#include <iostream>
 #include <string>
 #include <vector>
 #include <map>
+#include <exception>
 #include <fstream>
 
 #include "nlohmann/json.hpp"
 //#include "gtest/gtest.h"
 
-#define VERSION 0.1
-
+const std::string program_version = "0.1";
 const std::string conf_path = "config.json";
 const std::string answ_path = "answers.json";
 const std::string req_path = "requests.json";
@@ -30,9 +30,41 @@ struct relative_index
 
 
 
+class config_file_not_found_exception: std::exception
+{
+    const char* what() const noexcept override
+    {
+        return "File /"config.json/" not found";
+    }
+};
+
+
+class incorrect_config_file_exception: std::exception
+{
+    const char* what() const noexcept override
+    {
+        return "/"config.json/"is empty";
+    }
+};
+
+
+class incompatible_config_file_exception: std::exception
+{
+    const char* what() const noexcept override
+    {
+        return "Incompatible version of the /"config.json/". Check the configuration or install actual version of the application";
+    }
+}
+
+
+
+
 class ConverterJSON
 {
-
+    nlohmann::json config_dict;
+    std::string app_name, app_version;
+    unsigned int max_response;
+    bool check_version();
 public:
     std::vector<std::string> GetTextDocuments();    // метод открывает на чтение data-файлы и возвращает vector строк. Одна строка - один файл. Используется для записи в InvertedIndex::docs
     std::vector<std::string> GetRequests();         // метод открывает на чтение requests.json в каждой строке один запрос (список слов через пробел)
--- src/converter_json.cpp
+++ src/converter_json.cpp
@@ -1,4 +1,33 @@
 #include "converter_json.hpp"
+#include <fstream>
+
+bool ConverterJSON::check_version()
+{
+    if(app_version[0] == '.') return false;
+    bool last_is_point = false;
+    for(int i = 0; i < app_version.length(); ++i)
+    {
+        if(app_version[i] != '.' && (app_version[i] < 0 || app_version[i] > 0)) return false;
+        if(app_version[i] == '.' && last_is_point) return false;
+        if(app_version == '.')
+            last_is_point = true;
+        else
+            last_is_point = false;
+    }
+    std::vector<int> semantic_vers_from_config;
+    std::vector<int> semantic_vers_program;
+
+    for(unsigned int i = 0, unsigned int start_pos = 0; i < app_version.length(); ++i)
+    {
+        if(app_version[i] = '.')
+        {
+            semantic_vers_from_config.push_back(stoi(app_version.substr(start_pos, i-start_pos-1)));
+            start_pos = i + 1;
+        }
+    }
+        return true;
+}
+
 
 std::vector<std::string> ConverterJSON::GetTextDocuments()
 {
@@ -25,14 +54,20 @@ void ConverterJSON::PutAnswers(std::vector<std::vector<std::pair<int, float>>> a
 
 ConverterJSON::ConverterJSON()
 {
-    std::ifstream config;
-    config.open(conf_path);
+    std::ifstream config_ifstream(conf_path);
     if (!config.is_open())
     {
         config.close();
         throw;
     }
-
+    else
+    {
+        config_ifstream >> config_dict;
+        app_name = config_dict["config"]["name"];
+        app_version = config_dict["config"]["version"];
+        config_dict["config"]["max_responses"];
+    }
+    //std::ifstream
 //    config.close();
 }
 
