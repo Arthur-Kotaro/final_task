1714335238d1e2864f700510b3abab2b5ecf936a
Arthur-Kotaro <85132158+Arthur-Kotaro@users.noreply.github.com>
2026-01-10 16:19:44 +0400

    Indexing completed

--- inc/search_server.hpp
+++ inc/search_server.hpp
@@ -1,15 +1,15 @@
 #pragma once
 
 #include <iostream>
-#include <cstddef>
+#include <fstream>
 #include <string>
 #include <vector>
 #include <future>
+#include <map>
+#include <utility>
+#include <cstddef>
 #include <algorithm>
 //#include <thread>
-#include <map>
-//#include <exception>
-#include <fstream>
 
 #include "converter_json.hpp"
 
@@ -28,28 +28,30 @@ struct relative_index
 class inverted_index
 {
 private:
-    std::vector<std::string> docs;                  // Каждый объект типа string это список слов из одного файла. Индекс соответствует... например docs[0] = “milk sugar salt”;
-    std::map<std::string, std::vector<entry>> * freq_dictionary; // “milk”, {0, 1} “слово из запроса”, {индекс файла из docs, количество вхождений};
-    ConverterJSON * converter_json_ptr;
+    std::vector<std::string> docs;                                                   // Каждый объект типа string это список слов из одного файла. Индекс соответствует... например docs[0] = “milk sugar salt”;
+    std::unique_ptr<std::map<std::string, std::vector<entry>>> freq_dictionary;    // “milk”, {0, 1} “слово из запроса”, {индекс файла из docs, количество вхождений};
+//    std::map<std::string, std::vector<entry>> *freq_dictionary{};
+//    ConverterJSON * converter_json_ptr;
+
+    static std::unique_ptr<std::map<std::string, std::vector<entry>>> separate_indexing(int _doc_id, const std::string & txt_file_content);
+    static void merge_auxiliary_maps(std::vector<std::unique_ptr<std::map<std::string, std::vector<entry>>>> &auxiliary_maps, unsigned int hardware_threads);
+    static void merge_two_separated_maps(std::vector<std::unique_ptr<std::map<std::string, std::vector<entry>>>> &sep_index_vec, size_t dst, size_t src);
+    static void merge_two_sorted_index_vec(std::vector<entry> &dst, std::vector<entry> &src);
 
 public:
-    inverted_index(ConverterJSON * _converter_json);
+    explicit inverted_index(ConverterJSON * _converter_json);
     ~inverted_index();
-//    void update_document_base(std::vector<std::string> input_docs);
-//    static std::map<std::string, std::vector<entry>> * separate_indexing(int _doc_id, const std::string & txt_file_content);
-    static std::unique_ptr<std::map<std::string, std::vector<entry>>> separate_indexing(int _doc_id, const std::string & txt_file_content);
-
-    void update_document_base();
-    std::vector<entry> get_word_count(const std::string &word);
+    void update_document_base(std::vector<std::string> input_docs);
+    static std::vector<entry> get_word_count(const std::string &word);
 };
 
 
 class search_server
 {
 private:
-    inverted_index _index;
+    inverted_index & _index;
 
 public:
-    explicit search_server(inverted_index &idx);
+    explicit search_server(inverted_index& idx): _index(idx) {}
     std::vector<std::vector<relative_index>> search(const std::vector<std::string>& queries_input);
 };
--- src/search_server.cpp
+++ src/search_server.cpp
@@ -1,15 +1,61 @@
 #include "search_server.hpp"
-#define DEBUG
+//#define DEBUG
 
 inverted_index::inverted_index(ConverterJSON * _converter_json)
 {
-    converter_json_ptr = _converter_json;
-    update_document_base();
+//    converter_json_ptr = _converter_json;
+    update_document_base(_converter_json->GetTextDocuments());
 }
 
-//std::map<std::string, std::vector<entry>> * inverted_index::separate_indexing(int _doc_id, const std::string & txt_file_content)
-std::unique_ptr<std::map<std::string, std::vector<entry>>> inverted_index::separate_indexing(int _doc_id, const std::string & txt_file_content)
 
+void inverted_index::update_document_base(std::vector<std::string> input_docs)
+{
+#ifdef DEBUG
+    std::cout << "\n\"inverted_index::update_document_base() \" called\n";
+#endif
+    docs = std::move(input_docs);
+
+    if(docs.empty()) return;
+//    if(!freq_dictionary->empty()) freq_dictionary->clear();
+    std::vector<std::future<std::unique_ptr<std::map<std::string, std::vector<entry>>>>> index_future;
+    std::vector<std::unique_ptr<std::map<std::string, std::vector<entry>>>> index_for_each_file;
+    index_future.reserve(docs.size());
+    index_for_each_file.reserve(docs.size());
+
+    for (size_t i = 0; i < docs.size(); ++i)
+    {
+        index_future.push_back(std::async(std::launch::async, &inverted_index::separate_indexing, static_cast<int>(i), std::cref(docs[i])));
+    }
+    for (size_t i = 0; i < docs.size(); ++i)
+    {
+        index_for_each_file.push_back(index_future[i].get());
+    }
+
+    //sort vector in descending order
+    std::sort(begin(index_for_each_file), end(index_for_each_file), [](std::unique_ptr<std::map<std::string, std::vector<entry>>> &left, std::unique_ptr<std::map<std::string, std::vector<entry>>> &right) ->bool
+    { return  left->size() > right->size();});
+#ifdef DEBUG
+    std::cout << "\n\"After sorting of index_for_each_file vector\"\n";
+    size_t sum = 0;
+    for(size_t i = 0; i < index_for_each_file.size(); ++i)
+    {
+        std::cout << index_for_each_file[i]->size() << ' ';
+        sum += index_for_each_file[i]->size();
+    }
+    std::cout << std::endl << "Sum of map.size() " << sum << std::endl;
+#endif
+    //merge maps and free memory
+    const unsigned int hardware_threads = std::thread::hardware_concurrency() ? std::thread::hardware_concurrency() : 2;
+    merge_auxiliary_maps(index_for_each_file, hardware_threads);
+
+#ifdef DEBUG
+    std::cout << "\nAuxiliary maps merged. Auxiliary maps vector size = " << index_for_each_file.size() << ". Size of resulting map = " << index_for_each_file[0]->size() << ".\n";
+#endif
+    freq_dictionary = std::move(index_for_each_file[0]);
+}
+
+
+std::unique_ptr<std::map<std::string, std::vector<entry>>> inverted_index::separate_indexing(int _doc_id, const std::string & txt_file_content)
 {
     auto separated_map = std::make_unique<std::map<std::string, std::vector<entry>>>();
     const char *start    = txt_file_content.c_str();
@@ -31,64 +77,69 @@ std::unique_ptr<std::map<std::string, std::vector<entry>>> inverted_index::separ
     return separated_map;
 }
 
-
-void inverted_index::update_document_base()
+void inverted_index::merge_auxiliary_maps(std::vector<std::unique_ptr<std::map<std::string, std::vector<entry>>>> &auxiliary_maps, const unsigned int hardware_threads)
 {
-#ifdef DEBUG
-    std::cout << "\n\"inverted_index::update_document_base() \" called\n";
-#endif
-
-    docs = converter_json_ptr->GetTextDocuments();
-    std::vector<std::future<std::unique_ptr<std::map<std::string, std::vector<entry>>>>> index_future;
-    std::vector<std::unique_ptr<std::map<std::string, std::vector<entry>>>> index_for_each_file;
-    index_future.reserve(docs.size());
-    index_for_each_file.reserve(docs.size());
-
-
-//    std::string example("some text");
-//    int ex = 5;
-    for (size_t i = 0; i < docs.size(); ++i)
+    if (auxiliary_maps.size() == 1) return;
+    unsigned int threads_num = (auxiliary_maps.size() / 2) >= hardware_threads ? hardware_threads : (auxiliary_maps.size() / 2);
+    std::vector<std::thread> threads;
+    threads.reserve(threads_num);
+    for (size_t i = 0; i < threads_num; ++i)
     {
-//        index_future.push_back(std::async(std::launch::async, &inverted_index::separate_indexing, ex, example));
-        index_future.push_back(std::async(std::launch::async, &inverted_index::separate_indexing, static_cast<int>(i), std::cref(docs[i])));
-//        index_future.push_back(std::async(std::launch::async, &inverted_index::separate_indexing, this, static_cast<int>(i), std::cref(docs[i])));
-
-//        index_for_each_file_ftr[i] = std::async(std::launch::async, [this, &i]() -> std::future<std::map<std::string, std::vector<entry>> *> { return separate_indexing(i, docs[i]);});
+        threads.emplace_back(merge_two_separated_maps, std::ref(auxiliary_maps), i, auxiliary_maps.size() - i - 1);
     }
-    for (size_t i = 0; i < docs.size(); ++i)
+    for (auto &it: threads)
     {
-        index_for_each_file.push_back(index_future[i].get());
+        if (it.joinable()) it.join();
     }
-
-    //sort vector in ascending order
-
-#ifdef DEBUG
-    std::cout << "\n\"Sorting of index_for_each_file array\"\n";
-    for(size_t i = 0; i < docs.size(); ++i) std::cout << index_for_each_file[i]->size() << ' ';
-    std::cout << std::endl;
-#endif
-
-    std::sort(begin(index_for_each_file), end(index_for_each_file), [](std::unique_ptr<std::map<std::string, std::vector<entry>>> &left, std::unique_ptr<std::map<std::string, std::vector<entry>>> &right) ->bool
-    { return  left->size() > right->size();});
-
-#ifdef DEBUG
-    std::cout << "\n\"Sorting of index_for_each_file array\"\n";
-    for(size_t i = 0; i < docs.size(); ++i) std::cout << index_for_each_file[i]->size() << ' ';
-    std::cout << std::endl;
-#endif
-
-    //merge maps and free memory
-
-//    for(size_t i = 0; i < docs.size()-1; ++i)
-//    {
-//        delete index_for_each_file[i];
-//    }
-//    freq_dictionary = index_for_each_file[docs.size()-1];
+    for (int i = 0; i < threads_num; ++i)
+    {
+        auxiliary_maps.pop_back();
+    }
+    if (auxiliary_maps.size() > 1)
+    {
+        merge_auxiliary_maps(auxiliary_maps, hardware_threads);
+    }
 }
 
-
-search_server::search_server(inverted_index &idx): _index(idx)
+void inverted_index::merge_two_separated_maps(std::vector<std::unique_ptr<std::map<std::string, std::vector<entry>>>> &sep_index_vec, size_t dst, size_t src)
 {
-
+#ifdef DEBUG
+    std::cout << "\n\"Function inverted_index::merge_separate_map called. Destination index = " << dst << ", source index = " << src << "\"\n";
+#endif
+    for (auto it = sep_index_vec[src]->begin();  it != sep_index_vec[src]->end();)
+    {
+        auto it_next = std::next(it);
+        if (!sep_index_vec[dst]->contains(it->first))
+        {
+            auto tmp = sep_index_vec[src]->extract(it);
+            sep_index_vec[dst]->insert(std::move(tmp));
+        }
+        else
+        {
+            merge_two_sorted_index_vec(sep_index_vec[dst]->operator[](it->first), sep_index_vec[src]->operator[](it->first));
+        }
+        it = it_next;
+    }
 }
 
+void inverted_index::merge_two_sorted_index_vec(std::vector<entry> &dst, std::vector<entry> &src)
+{
+    if(src.empty()) return;
+    std::sort(src.begin(), src.end(), [](auto const &a, auto const &b) ->bool {return a.doc_id < b.doc_id;}); //exclude statement?
+    std::vector<entry> out;
+    out.reserve(dst.size() + src.size());
+    size_t i = 0, j = 0;
+    while ( i < dst.size() && j < src.size())
+    {
+        if(dst[i].doc_id < src[j].doc_id) out.push_back(std::move(dst[i++]));
+        else if (dst[i].doc_id > src[j].doc_id) out.push_back(std::move(src[j++]));
+    }
+    while (i < dst.size()) out.push_back(dst[i++]);
+    while (j < src.size()) out.push_back(src[j++]);
+    dst = std::move(out);
+}
+
+std::vector<entry> inverted_index::get_word_count(const std::string &word)
+{
+    return {};
+}
